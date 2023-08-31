c4ae123ef5e771b4365484d40fc7b20693f28cb6
Mayank Parihar <mayank.dev.parihar1988@gmail.com>
2023-08-31 08:50:41 +0530

    adding config and StocksApp to git

---
+++ ConfigurationSetting/ConfigurationSetting/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+    {
+    endpoints.Map("/", async context =>
+    {
+        await context.Response.WriteAsync(app.Configuration["MyKey"]+ "\n");
+        await context.Response.WriteAsync(app.Configuration.GetValue<string>("hello", "hi"));
+    });   
+});
+
+app.Run();
+
